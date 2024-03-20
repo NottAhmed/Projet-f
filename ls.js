fd7c0f66ef9142422484b84878366a7ac4e8bde0
NottAhmed <119789649+NottAhmed@users.noreply.github.com>
2024-03-20 15:47:32 +0100

    Update ls.js

--- ls.js
+++ ls.js
@@ -1,15 +1,12 @@
-function loginvalid(){
-    var adr = document.getElementById('email');
-    var atIndex = adr.indexOf('@');
-    var dotIndex = adr.lastIndexOf('.');
+   function loginvalid() {
+            var adr = document.getElementById('email').value; // Access value of the input field
+            var atIndex = adr.indexOf('@');
+            var dotIndex = adr.lastIndexOf('.');
 
-    if (atIndex < 1 || dotIndex < atIndex + 2 || dotIndex + 2 >= adr.length) {
-        alert("Invalid email");
-        
-    } else {
-        alert("Email is valid");
-      
-    }
-    
-}
+            if (atIndex < 1 || dotIndex < atIndex + 2 || dotIndex + 2 >= adr.length) {
+                alert("Invalid email");
+            } else {
+                alert("Email is valid");
+            }
+        }
 
