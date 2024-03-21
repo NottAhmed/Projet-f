31cdb0f8b412f8984a5a754ea62eb0efa535e57b
NottAhmed <119789649+NottAhmed@users.noreply.github.com>
2024-03-21 20:29:17 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function search(){
+    var s= document.getElementById('search').value;
+ 
+    var Term = s.trim();
+
+if (Term !== '') {
+    
+
+    alert(Term)
+
+    
+}else{
+
+
+
+}
+
+
+
+    }
