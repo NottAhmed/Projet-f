1a8ab8f3f53efce98e61feee195dfcff682bad98
Nourhenezroud <164028510+Nourhenezroud@users.noreply.github.com>
2024-03-27 00:41:25 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,43 @@
+var threads=[
+
+   {
+    id: 1,
+    title : "thread 1",
+    author: "Aaron",
+    date: Date.now(),
+    content:"thread content",
+    comments:[
+        {
+            author:"jack",
+            date:Date.now(),
+            content :"hey there "
+        },
+        {
+            author:"Arthur",
+            date:Date.now(),
+            content :"hey to you too " 
+        }
+    ]
+   },  
+   {
+    id: 2,
+    title : "thread 2",
+    author: "Aaron",
+    date: Date.now(),
+    content:"thread content",
+    comments:[
+        {
+            author:"jack",
+            date:Date.now(),
+            content :"hey there "
+        },
+        {
+            author:"Arthur",
+            date:Date.now(),
+            content :"hey to you too " 
+        }
+    ]
+   }
+
+
+]
