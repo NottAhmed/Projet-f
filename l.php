2750adceaf9a5c3a932943cca9c9120c537a8d99
nawres Mabrouk <130879130+nawressmabrouk@users.noreply.github.com>
2024-04-14 01:55:09 +0100

    Update l.php

--- l.php
+++ l.php
@@ -1,4 +1,52 @@
 <?php
+/*hedha shih ama hatyneh fi commentaire juste bch l code yo9od yekhdm aal github
+
+session_start(); // Start the session
+
+// Database connection parameters
+$servername = "localhost";
+$username = "root"; // Change this to your MySQL username
+$password = ""; // Change this to your MySQL password
+$dbname = "savoir"; // Change this to your database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$email = $_POST['email'];
+$password = $_POST['pass'];
+
+
+$sql = "SELECT * FROM etudiant WHERE email='$email' AND password='$password'";
+
+
+// Execute SQL query
+$result = $conn->query($sql);
+
+// Check if any rows are returned
+if ($result->num_rows > 0) {
+    // Matching record found
+    $row = $result->fetch_assoc();
+
+    // Store data in session variables
+    $_SESSION['email'] = $row['email']; // Set email in session
+
+    echo "etudiant found";
+    echo "<script>setTimeout(function() { window.location.href = 'main.html'; }, 2000);</script>";
+} else {
+    // No matching record found
+    echo "etudiant not found";
+    echo "<script>setTimeout(function() { window.location.href = 'sl.html'; }, 2000);</script>";
+}
+
+// Close connection
+$conn->close();
+*/
+
 // Database connection parameters
 $servername = "localhost";
 $username = "root"; // Change this to your MySQL username
@@ -38,4 +86,5 @@ if ($result->num_rows > 0) {
 
 // Close connection
 $conn->close();
+
 ?>
